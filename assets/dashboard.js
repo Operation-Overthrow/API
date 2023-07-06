e87abd396ed22f54da1d1c99f900ad6b2f3c30a8
Melaine Gérard <pro@melaine-gerard.fr>
2023-07-06 13:44:09 +0200

    :sparkles: Ajout dashboard

---
+++ assets/dashboard.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    window.location.replace("/dashboard/login");
+  }
+
+  const request = fetch("/api/games", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  request
+    .then((response) => {
+      if (response.status === 200) {
+        const gamesList = document.querySelector("#games-list");
+        response.json().then((data) => {
+          if (data.length !== 0) {
+            gamesList.innerHTML = "";
+            data.forEach((game) => {
+              const gameRow = document.createElement("tr");
+              const gameId = document.createElement("th");
+              const gameDate = document.createElement("td");
+              const gameScore = document.createElement("td");
+
+              gameId.setAttribute("scope", "row");
+
+              gameId.innerHTML = game.id;
+              gameDate.innerHTML = "TODO";
+              gameScore.innerHTML = game.score;
+
+              gameRow.appendChild(gameId);
+              gameRow.appendChild(gameDate);
+              gameRow.appendChild(gameScore);
+
+              gamesList.appendChild(gameRow);
+            });
+          }
+        });
+      } else {
+        window.location.replace("/dashboard/login");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      window.location.replace("/dashboard/login");
+    });
+});
